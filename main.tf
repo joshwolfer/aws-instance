465118c80c6be20a475e01fa427f75696989299f
Josh Wolfer <josh@bigdumbmonkey.com>
2019-11-08 15:15:48 -0600

    add production scale

--- main.tf
+++ main.tf
@@ -3,12 +3,16 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
+  count = 2
+
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
+  key_name = "jwolfer"
 
   tags = {
     Name = "joshwolfer"
     Owner = "jwolfer@hashicorp.com"
+    Branch = "prod"
   }
 }
