3ac1690d03a2eebb5fa060f5b90c53cc21d42d52
Josh Wolfer <josh@bigdumbmonkey.com>
2019-11-08 14:49:22 -0600

    change key to jwolfer

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
+  key_name = "jwolfer"
 
   tags = {
     Name = "joshwolfer"
